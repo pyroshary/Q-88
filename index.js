d26012fcaea17daedd11822ecfc8935b5d7b630f
pyroshary <sheharyarghulamrasool@gmail.com>
2024-04-08 02:02:01 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+function roundtoNearest(num) {
+    return Math.round(num);
+}
+console.log(roundtoNearest(5.6));
+console.log(roundtoNearest(5.4));
---
+++ index.ts
@@ -0,0 +1,8 @@
+ //making a function to round off the number from points
+ 
+ function roundtoNearest(num:number):number{
+    return Math.round(num);
+ }
+
+ console.log(roundtoNearest(5.6));
+ console.log(roundtoNearest(5.4));
